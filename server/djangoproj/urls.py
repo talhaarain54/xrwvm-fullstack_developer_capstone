4955a0f2500c450f212ff94ad864ca151b7a0ad7
talhaarain54 <araintalha0304@gmail.com>
2025-12-18 02:03:28 -0500

    Module 4 completed

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -26,5 +26,11 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),    
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+
+
+    path('<path:any>/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
