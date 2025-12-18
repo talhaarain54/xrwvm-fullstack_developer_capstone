7027a1149acd2190e393672d88d76e09c0351ac6
Muhammad Talha Shafiq <araintalha0304@gmail.com>
2025-12-18 18:52:43 +0500

    Code linting issues fixed

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,23 +1,31 @@
-from django.urls import path
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
 from . import views
 
-app_name = 'djangoapp'
+app_name = "djangoapp"
+
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_request, name='logout'),
-    path(route='register', view=views.registration, name='register'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    # path for dealer reviews view
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),    
-    path(route='add_review', view=views.add_review, name='add_review'),
-    # path for add a review view
-
+    path("login", views.login_user, name="login"),
+    path("logout", views.logout_request, name="logout"),
+    path("register", views.registration, name="register"),
+    path("get_cars", views.get_cars, name="get_cars"),
+    path("get_dealers", views.get_dealerships, name="get_dealers"),
+    path(
+        "get_dealers/<str:state>",
+        views.get_dealerships,
+        name="get_dealers_by_state",
+    ),
+    path(
+        "dealer/<int:dealer_id>",
+        views.get_dealer_details,
+        name="dealer_details",
+    ),
+    path(
+        "reviews/dealer/<int:dealer_id>",
+        views.get_dealer_reviews,
+        name="dealer_reviews",
+    ),
+    path("add_review", views.add_review, name="add_review"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
